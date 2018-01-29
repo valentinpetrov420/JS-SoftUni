8ced4646fdaab146a4c4da66d7f72a195ce09243
amundire <valentin.petrov420@abv.bg>
2018-01-29 21:58:13 +0200

    asd

--- 06. (Lab) Arrays and Matrices (10)/05. Last K Sequence/lastksequence.js
+++ 06. (Lab) Arrays and Matrices (10)/05. Last K Sequence/lastksequence.js
@@ -1,13 +1,18 @@
-function solve(n, k) {
-    let sum = 0;
-    let seq = [1];
-    for (let current = 1; current < n; current++) {
-        let start = Math.max(0, current - k);
-        let end = current - 1;
-        if(current >= start && current <= end){
-            sum+=
-        }
+function solve(length, k) {
+    let outputArr = [1];
+    for(let i = 1; i<length; i++){
+        outputArr[i] = sumLastK(outputArr, k);
     }
-    console.log(seq.join(' '));
-}
+    console.log(outputArr.join(' '));
 
+    function sumLastK(arr, k) {
+        k = arr.length>k ? k : arr.length;
+        let sum = 0;
+        for(let i = 1; i<=k; i++){
+            sum += arr[arr.length-i];
+        }
+        return sum;
+    }
+}
+solve(6,3);
+// solver(6,7);
--- 09. Strings and Regex (10)/05. Capitalize Words/capitalizewords.js
+++ 09. Strings and Regex (10)/05. Capitalize Words/capitalizewords.js
@@ -1,13 +1,19 @@
-function solve(str){
-    let words = str.split(' ');
-    let result = '';
-    for (let i = 0; i < words.length; i++) {
-        let firstLetter = words[i].charAt(0);
-        let currentWord = words[i];
-        currentWord.substr(0, 1) = firstLetter.toUpperCase();
-        result += currentWord + ' ';
+function solve([input]) {
+    input = input.toUpperCase();
+    let result = [];
+    let letters = '';
+    let text = input.split(' ');
+
+    for (let element of text){
+        let word = element[0];
+        for (let i = 1; i < element.length; i++){
+            letters += element[i].replace(element[i], element[i].toLowerCase());
+        }
+        word += letters;
+        letters = '';
+        result.push(word);
     }
-    console.log(result);
+    console.log(result.join(' '));
 }
 
-solve('Asd dsa ghf');
+solve(['Asd dsa ghf']);
